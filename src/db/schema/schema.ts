b268cb3e6fe39a2f2086fd74f4d4a7bb5a07d0f2
abdelrahman-20 <abdelrahman.salem.1002@gmail.com>
2026-06-23 18:30:00 +0300

    Implement Database Schema

--- src/db/schema/schema.ts
+++ src/db/schema/schema.ts
@@ -1,50 +1,46 @@
-// import { relations } from "drizzle-orm";
-// import { pgTable, integer, timestamp, varchar } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
 
-import { integer, pgTable } from "drizzle-orm/pg-core";
+const timeStamps = {
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
+  updatedAt: timestamp("updatedAt")
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
+};
 
-// const timestamps = {
-//   createdAt: timestamp("createdAt").defaultNow().notNull(),
-//   updatedAt: timestamp("updatedAt")
-//     .defaultNow()
-//     .$onUpdate(() => new Date())
-//     .notNull(),
-// };
+export const departments = pgTable("departments", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  code: varchar("code", { length: 50 }).notNull().unique(),
+  name: varchar("name", { length: 255 }).notNull(),
+  description: varchar("description", { length: 255 }).notNull(),
+  ...timeStamps,
+});
 
-// export const departments = pgTable("departments", {
-//   id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
-//   code: varchar("code", { length: 50 }).notNull().unique(),
-//   name: varchar("name", { length: 255 }).notNull(),
-//   description: varchar("description", { length: 255 }).notNull(),
-//   ...timestamps,
-// });
+export const subjects = pgTable("subjects", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  departmentID: integer("departmentID")
+    .notNull()
+    .references(() => departments.id, { onDelete: "restrict" }),
+  code: varchar("code", { length: 50 }).notNull().unique(),
+  name: varchar("name", { length: 255 }).notNull(),
+  description: varchar("description", { length: 255 }).notNull(),
+  ...timeStamps,
+});
 
-// export const subjects = pgTable("subjects", {
-//   id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
-//   departmentId: integer("departmentId")
-//     .notNull()
-//     .references(() => departments.id, { onDelete: "restrict" }),
-//   code: varchar("code", { length: 50 }).notNull().unique(),
-//   name: varchar("name", { length: 255 }).notNull(),
-//   description: varchar("description", { length: 255 }).notNull(),
-//   ...timestamps,
-// });
+export const departmentRelations = relations(departments, ({ many }) => ({
+  subjects: many(subjects),
+}));
 
-// export const departmentRelations = relations(departments, ({ many }) => ({
-//   subjects: many(subjects),
-// }));
+export const subjectsRelations = relations(subjects, ({ one, many }) => ({
+  department: one(departments, {
+    fields: [subjects.departmentID],
+    references: [departments.id],
+  }),
+}));
 
-// export const subjectRelations = relations(subjects, ({ one, many }) => ({
-//   department: one(departments, {
-//     fields: [subjects.departmentId], // FK on subjects table
-//     references: [departments.id], // PK on departments table
-//   }),
-// }));
+export type Department = typeof departments.$inferSelect;
+export type NewDepartment = typeof departments.$inferInsert;
 
-// export type Department = typeof departments.$inferSelect;
-// export type NewDepartment = typeof departments.$inferInsert;
-
-// export type Subject = typeof subjects.$inferSelect;
-// export type NewSubject = typeof subjects.$inferInsert;
-
-// export const departments = pgTable("departments", {});
+export type Subject = typeof subjects.$inferSelect;
+export type NewSubject = typeof subjects.$inferInsert;
