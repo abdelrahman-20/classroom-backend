0deaf28327139696ba4c3fec6ee33a7075fe75cb
abdelrahman-20 <abdelrahman.salem.1002@gmail.com>
2026-09-01 19:24:45 +0300

    Add User Authentication.

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import { swaggerSpec } from "./config/swagger";
 import subjectsRouter from "./routes/subjectsRoutes";
 import errorHandler from "./middleware/errorHandler";
 import securityMiddleware from "./middleware/security";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 dotenv.config();
 
@@ -21,16 +23,46 @@ else if (!process.env.PORT) {
 const app = express();
 const PORT = process.env.PORT ? Number(process.env.PORT) : 8000;
 
-// Application Middlewares
+// Configure CORS
+const corsOrigins = [process.env.FRONTEND_URL];
+if (process.env.NODE_ENV !== "production") {
+  corsOrigins.push(
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173",
+  );
+}
+
+// Middleware to inject default Origin header for dev tools like API-Dog
+app.use((req, res, next) => {
+  if (!req.headers.origin && process.env.NODE_ENV !== "production") {
+    req.headers.origin = "http://localhost:3000";
+  }
+  next();
+});
+
+// Authentication Middleware "Better-Auth"
+app.all("/api/auth/{*any}", toNodeHandler(auth));
+
+// Additional Middlewares
 app.use(express.json());
 app.use(
   cors({
     // Enable CORS To Access The Server
-    origin: process.env.FRONTEND_URL,
+    origin: (origin, callback) => {
+      if (!origin || corsOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
 );
+
+// Security Middleware "ArcJet"
 app.use(securityMiddleware);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
